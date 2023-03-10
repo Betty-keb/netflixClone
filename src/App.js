922c3cf667926e97774024b0b7aa69029d6f94f7
Betelhem Kebede <betilove6@gmail.com>
2023-03-10 08:51:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import Banner from "./Banner";
+import Nav from "./Nav";
+import request from "./request";
+import Row from "./Row/Row";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={request.fetchNetflixOrginals}
+        isLargeRow
+        // isLargeRow={true}
+      />
+
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRatedMovies} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries Movies" fetchUrl={request.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
